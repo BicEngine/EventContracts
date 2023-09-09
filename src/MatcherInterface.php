d257e1368b730e859c577647d872c2814a44d0e7
SerafimArts <nesk@xakep.ru>
2023-09-09 16:42:15 +0000

    Add dispatcher component

--- src/MatcherInterface.php
+++ src/MatcherInterface.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Bic\Contracts\Event;
 
+/**
+ * @template TEvent of object
+ */
 interface MatcherInterface
 {
+    /**
+     * @param TEvent $event
+     */
     public function match(object $event): bool;
 }
